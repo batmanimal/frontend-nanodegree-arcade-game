71804a113b88d8294ac5df4517d7690e4f2c18f6
Jennie Kim <jk@udacity.com>
2014-10-27 16:04:08 -0700

    added comments

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
+// read the engine file 
+
 // Enemies our player must avoid
 var Enemy = function() {
+    // define the initial position and speed when you instantiate this enemy
+    // needs to be in x,y plane, define min and max 
+    // find min/max by looking at engine code (width and ht) where boundaries are defined 
+    // could make one ememy class and one player class 
+    
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -9,8 +16,13 @@ var Enemy = function() {
 }
 
 // Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Parameter: dt, a time delta between ticks -> value that normalizes for diff computer speeds 
+
+// create random number, take modulo of number div by numrows (get 0, 1,or 2) 
+// need to apply transformation to offset row coord with pixels (where the thing is actually going to show up)
+
 Enemy.prototype.update = function(dt) {
+    // this is predefined in the engine 
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -18,8 +30,8 @@ Enemy.prototype.update = function(dt) {
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); 
+} 
 
 // Now write your own player class
 // This class requires an update(), render() and
